5dd269756a33f4ad129115880b603f83a8fa14ed
Maks <33464332+Bars1704@users.noreply.github.com>
2021-09-11 17:13:04 +0300

    Initial Commit

---
+++ IOS-PixelArt-app/IOS_PixelArt_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_PixelArt_appApp.swift
+//  IOS-PixelArt-app
+//
+//  Created by Admin on 11.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct IOS_PixelArt_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
